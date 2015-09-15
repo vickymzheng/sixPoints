302f2ded3d03549b9e9750ee096f7b7724c5f5c0
Michael Drewitt <mikedrewitt@gmail.com>
2015-09-15 08:51:56 -0400

    Fix webpage for appengine push, Add web backend

---
+++ websitePitch/main.py
@@ -0,0 +1,34 @@
+import os
+import re
+from string import letters
+
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+
+
+class Handler(webapp2.RequestHandler):
+	
+	def write(self, *a, **kw):
+   		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+   		t = jinja_env.get_template(template)
+   		return t.render(params)
+	
+	def render(self, template, **kw):
+   		self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+	def get(self):
+		self.render('homePage.html', title = 'Driver')
+
+app = webapp2.WSGIApplication([('/', MainPage),
+								('/webHandler', Handler)],
+                              debug=True)
