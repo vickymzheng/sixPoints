893cb040b5dc716ca91f8d0f4626331885edacb9
paulkowa <paulkowa@buffalo.edu>
2015-10-16 00:13:38 -0400

    Updated for submission

--- app/src/main/java/app/sixpts/Main.java
+++ app/src/main/java/app/sixpts/Main.java
@@ -1,19 +1,33 @@
 package app.sixpts;
 
+import android.content.res.Resources;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.lang.reflect.Field;
 
 public class Main extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Spinner lotSpinner;
+    Spinner daySpinner;
+    Spinner hourSpinner;
+    Spinner minuteSpinner;
+    Spinner apmSpinner;
+    Spinner monthSpinner;
+    TimePicker timePicker;
+    Resources system;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +35,35 @@ public class Main extends AppCompatActivity implements AdapterView.OnItemSelecte
         setContentView(R.layout.activity_main);
 
         lotSpinner = (Spinner) findViewById(R.id.mapSpinner);
-        ArrayAdapter mapAdapter = ArrayAdapter.createFromResource(this, R.array.map_array, R.layout.spinner_layout);
+        ArrayAdapter mapAdapter = ArrayAdapter.createFromResource(this, R.array.map_array, R.layout.lotspinner_layout);
         lotSpinner.setAdapter(mapAdapter);
         lotSpinner.setOnItemSelectedListener(this);
+
+        daySpinner = (Spinner) findViewById(R.id.daySpinner);
+        ArrayAdapter dayAdapter = ArrayAdapter.createFromResource(this, R.array.day_array, R.layout.dayspinner_layout);
+        daySpinner.setAdapter(dayAdapter);
+        daySpinner.setOnItemSelectedListener(this);
+
+        hourSpinner = (Spinner) findViewById(R.id.hourSpinner);
+        ArrayAdapter hourAdapter = ArrayAdapter.createFromResource(this, R.array.hour_array, R.layout.hourspinner_layout);
+        hourSpinner.setAdapter(hourAdapter);
+        hourSpinner.setOnItemSelectedListener(this);
+
+        minuteSpinner = (Spinner) findViewById(R.id.minuteSpinner);
+        ArrayAdapter minuteAdapter = ArrayAdapter.createFromResource(this, R.array.minute_array, R.layout.minutespinner_layout);
+        minuteSpinner.setAdapter(minuteAdapter);
+        minuteSpinner.setOnItemSelectedListener(this);
+
+        apmSpinner = (Spinner) findViewById(R.id.apmSpinner);
+        ArrayAdapter apmAdapter = ArrayAdapter.createFromResource(this, R.array.apm_array, R.layout.apmspinner_layout);
+        apmSpinner.setAdapter(apmAdapter);
+        apmSpinner.setOnItemSelectedListener(this);
+
+        monthSpinner = (Spinner) findViewById(R.id.monthSpinner);
+        ArrayAdapter monthAdapter = ArrayAdapter.createFromResource(this, R.array.month_array, R.layout.monthspinner_layout);
+        monthSpinner.setAdapter(monthAdapter);
+        monthSpinner.setOnItemSelectedListener(this);
+
     }
 
     @Override
@@ -51,7 +91,7 @@ public class Main extends AppCompatActivity implements AdapterView.OnItemSelecte
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         TextView myText = (TextView) view;
-        Toast.makeText(this, "You Selected ", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Selected " + lotSpinner.getSelectedItem(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
