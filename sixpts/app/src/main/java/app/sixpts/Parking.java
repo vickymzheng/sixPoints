27e600a8ef623c301890e508a5b6d8cda41eafc5
vickyzheng94 <vickyzheng94@gmail.com>
2015-12-08 15:21:43 -0500

    things for activity detector

---
+++ sixpts/app/src/main/java/app/sixpts/Parking.java
@@ -0,0 +1,13 @@
+package app.sixpts;
+
+/**
+ * Created by Vicky on 11/29/15.
+ * This class will be used to get the times that users are leaving so that we can pair them with incoming users
+ */
+public class Parking {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parked);
+    }
+}
