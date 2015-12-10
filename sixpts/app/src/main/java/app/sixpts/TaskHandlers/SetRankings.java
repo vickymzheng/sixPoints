032aae1c030294e66f2a1bf0f4a9e484d96879e6
paulkowa <paulkowa@buffalo.edu>
2015-12-10 10:40:32 -0500

    Made rating value update more aesthetic and fixed issue with suggested lots being mispelled

--- sixpts/app/src/main/java/app/sixpts/TaskHandlers/SetRankings.java
+++ sixpts/app/src/main/java/app/sixpts/TaskHandlers/SetRankings.java
@@ -4,7 +4,6 @@ import java.util.Set;
 import java.util.Iterator;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.Button;
@@ -20,7 +19,6 @@ import app.sixpts.CustomObjects.Tuple;
  */
 public class SetRankings extends AsyncTask<Void, Integer, String> {
     Context context;
-    ProgressDialog progressDialog;
     Activity checkActivity;
     CheckAvail checkAvail;
     Button button;
@@ -36,27 +34,56 @@ public class SetRankings extends AsyncTask<Void, Integer, String> {
 
     @Override
     protected String doInBackground(Void... params) {
-        int i = 0;
         synchronized (this) {
             data.setTuple(findBest());
-            while (i < 10) {
+            checkActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    checkAvail.setBestLot(data.getTuple().getSuggestedLot());
+                }
+            });
+
+            while (checkAvail.getSelectedBar() != data.getTuple().getSelectedLotValue() || checkAvail.getSuggestedBar() != data.getTuple().getSuggestedLotValue()) {
                 try {
-                    wait(50);
-                    i++;
+                    wait(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                if (checkAvail.getSelectedBar() < data.getTuple().getSelectedLotValue()) {
                     checkActivity.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            checkAvail.setBestLot(data.getTuple().getSuggestedLot());
-                            checkAvail.setSelectedBar(data.getTuple().getSelectedLotValue());
-                            checkAvail.setSuggestedBar(data.getTuple().getSuggestedLotValue());
+                            checkAvail.setSelectedBar(checkAvail.getSelectedBar() + 1);
                             checkAvail.updateSelectedBarText();
+                        }
+                    });
+                } else if (checkAvail.getSelectedBar() > data.getTuple().getSelectedLotValue()) {
+                    checkActivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            checkAvail.setSelectedBar(checkAvail.getSelectedBar() - 1);
+                            checkAvail.updateSelectedBarText();
+                        }
+                    });
+                }
+
+                if (checkAvail.getSuggestedBar() < data.getTuple().getSuggestedLotValue()) {
+                    checkActivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            checkAvail.setSuggestedBar(checkAvail.getSuggestedBar() + 1);
                             checkAvail.updateSuggestedBarText();
                         }
                     });
-
-                    publishProgress(i);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                } else if (checkAvail.getSuggestedBar() > data.getTuple().getSuggestedLotValue()) {
+                    checkActivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            checkAvail.setSuggestedBar(checkAvail.getSuggestedBar() - 1);
+                            checkAvail.updateSuggestedBarText();
+                        }
+                    });
                 }
             }
         }
@@ -65,26 +92,17 @@ public class SetRankings extends AsyncTask<Void, Integer, String> {
 
     @Override
     protected void onPreExecute() {
-        progressDialog = new ProgressDialog(context);
-        progressDialog.setTitle("Retrieving data...");
-        progressDialog.setMax(10);
-        progressDialog.setProgress(0);
-        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        progressDialog.show();
     }
 
     @Override
     protected void onPostExecute(String result){
         Toast.makeText(context, result, Toast.LENGTH_LONG).show();
         button.setEnabled(true);
-        progressDialog.hide();
         this.cancel(true);
     }
 
     @Override
     protected void onProgressUpdate(Integer... values) {
-        int progress = values[0];
-        progressDialog.setProgress(progress);
     }
 
     private Tuple findBest() {
@@ -107,21 +125,16 @@ public class SetRankings extends AsyncTask<Void, Integer, String> {
             int value = data.getMap().get(current).get(String.valueOf(data.getMonth()) + String.valueOf(data.getDay()) + String.valueOf(data.getHour()) + String.valueOf(data.getMinute()) + String.valueOf(data.getAmp()));
             if(value < stats.getSuggestedLotValue()) {
                 stats.setSuggestedLotValue(value);
-                stats.setSuggestedLot(current);
+                stats.setSuggestedLot(fixLotName(current));
             }
-            /**
-             Set dates = data.getMap().get(current).keySet();
-             Iterator dateIter = dates.iterator();
-             // Iterate through key values in lot lists
-             while (dateIter.hasNext()) {
-             String time = dateIter.next().toString();
-             if (data.getMap().get(current).get(time) < stats.getSuggestedLotValue()) {
-             stats.setSuggestedLotValue(data.getMap().get(current).get(time));
-             stats.setSuggestedLot(current);
-             }
-             }
-             **/
         }
         return stats;
+    }
+
+    private String fixLotName(String lot) {
+        if (lot.charAt(lot.length() - 1) > 64 && lot.charAt(lot.length() - 1) < 91) {
+            return lot.substring(0, lot.length() - 1) + " " + lot.charAt(lot.length() - 1);
+        }
+        return lot;
     }
 }
--- sixpts/app/src/main/java/app/sixpts/messaging/send.java
+++
@@ -1,7 +0,0 @@
-package app.sixpts.messaging;
-
-/**
- * Created by paulkowa on 11/27/15.
- */
-public class send {
-}
